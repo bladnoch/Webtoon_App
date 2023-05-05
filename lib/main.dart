1fc96dbdaf901fb1b035ca76aa5bba3833e45b38
DK <dongwook5478@gmail.com>
2023-05-05 21:31:59 +0900

    save before use API

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:apptoon/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,11 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-      ),
-      home: Container(),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        elevation: 2,
+        title: Text("오늘의 웹툰",
+        style: TextStyle(fontSize: 24,
+        fontWeight: FontWeight.w400,),
+        ),
+      ),
+    );
+  }
+}
