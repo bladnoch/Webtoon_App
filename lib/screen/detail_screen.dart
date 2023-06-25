42deaff743e7469f5d0e6bd92180263e8b74d398
DK <dongwook5478@gmail.com>
2023-06-25 23:14:35 +0900

    add about, age

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -82,11 +82,37 @@ class _DetailScreenState extends State<DetailScreen> {
               ),
             ],
           ),
+          const SizedBox(
+            height: 25,
+
+          ),
           FutureBuilder(
             future: webtoon,
             builder: (context,snapshot){
               if(snapshot.hasData) {
-                return Text(snapshot.data!.about);
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(snapshot.data!.about,
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text('${snapshot.data!.genre} / ${snapshot.data!.age}',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
               }
               return Text("...");
             },
