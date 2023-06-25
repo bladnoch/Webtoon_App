a27d685034f0fae331aa22b57dfc310857cdc9a3
DK <dongwook5478@gmail.com>
2023-06-25 20:46:10 +0900

    add Hero() to add detail of floating image

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -38,20 +38,23 @@ class DetailScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 250,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15,
-                        offset: Offset(10,10),
-                        color: Colors.black.withOpacity(0.5),
-                      ),
-                    ]
+              Hero(
+                tag: id,
+                child: Container(
+                  width: 250,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 15,
+                          offset: Offset(10,10),
+                          color: Colors.black.withOpacity(0.5),
+                        ),
+                      ]
+                  ),
+                  child: Image.network(thumb),
                 ),
-                child: Image.network(thumb),
               ),
             ],
           ),
